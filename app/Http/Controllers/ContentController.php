9cc8380acf86fe72b570cb9bdc105b0237fb7b05
WalterFrongia <walter.frongia@libero.it>
2021-11-06 15:31:20 +0100

    Dichiarata variabile per richiamare il db con il model e mandata alla view con data

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Travel;
 
 class ContentController extends Controller
 {
     public function index() {
-        
-        return view('content');
+        //inizializziamo e dichiariamo una variabile che richiama il model e aggiungiamo ::all per prendere tutti i dati del db con model. 
+        $travel = Travel::all();
+        dd($travel); //serve per stampare array nel browers.
+
+        //con data mandiamo i dati alla view e poi scriviamo il valore in return. 
+        $data=[
+            "travel" => $travel
+        ];
+        return view('content',$data);
     }
 }
